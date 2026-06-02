a39a9d711f462b8b3d1ed49dd80b408e80cde932
Caio <cbrunofb@gmail.com>
2026-06-02 16:03:33 -0300

    boot: switch to linuxPackages_latest, set timeout 0

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: {
   boot = {
-    kernelPackages = pkgs.linuxPackages;
+    kernelPackages = pkgs.linuxPackages_latest;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      timeout = 0;
     };
     supportedFilesystems = ["btrfs" "ntfs"];
     initrd = {
