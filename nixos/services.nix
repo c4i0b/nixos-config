e55ad14501b19c456486458a4490e41ee4138530
Caio <cbrunofb@gmail.com>
2026-06-02 16:03:43 -0300

    services: add topgrade timer, fix nix-cleanup, add nix-generations

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,5 +1,33 @@
 {pkgs, ...}: {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      nix-rebuild = "git -C /etc/nixos add -A && sudo nixos-rebuild switch --flake /etc/nixos";
+      nix-cleanup = "sudo nix-collect-garbage -d && sudo nixos-rebuild boot --flake /etc/nixos";
+      nix-generations = "nixos-rebuild list-generations";
+      nix-rollback = "sudo nixos-rebuild switch --rollback";
+    };
+  };
+
+  systemd.user.services.topgrade = {
+    description = "Topgrade";
+    serviceConfig = {
+      Type = "oneshot";
+      Nice = 19;
+      IOSchedulingClass = "idle";
+    };
+    script = "${pkgs.topgrade}/bin/topgrade --disable system";
+  };
+
+  systemd.user.timers.topgrade = {
+    description = "Daily topgrade";
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      RandomizedDelaySec = "1h";
+    };
+  };
 
   services.openssh = {
     enable = true;
