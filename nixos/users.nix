1be99d8d24a9794dc1eb9cf531b67d7de2081ca5
Caio <cbrunofb@gmail.com>
2026-06-02 10:43:57 -0300

    Set fish as default shell for user caio

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,8 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   users.users = {
     caio = {
       initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
+      shell = pkgs.fish;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII36LvhYgcFFJgpr3/lrnD7z/zp0EKBn2HFUep/0DiEZ"
       ];
