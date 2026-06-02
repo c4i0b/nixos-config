94bf3db78e680aedc2d73f732d198dc492052bdc
Caio <cbrunofb@gmail.com>
2026-06-02 10:17:43 -0300

    Switch from linuxPackages_zen to linuxPackages (default vanilla kernel)

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.linuxPackages;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
