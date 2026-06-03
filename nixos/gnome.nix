24e6a4002e3bd6e7265c80b1e3f3448decf87b79
Caio <cbrunofb@gmail.com>
2026-06-03 18:37:59 -0300

    gnome: dconf profile with keybindings, power, extensions and nautilus settings

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -39,145 +39,176 @@
 
   programs.dconf.profiles.user.databases = [
     {
-      settings =
-        let
-          inherit (lib.gvariant) mkTuple mkUint32 mkBoolean mkString;
-        in
-        {
-          "org/gnome/desktop/interface" = {
-            color-scheme = "prefer-dark";
-            accent-color = "teal";
-            font-name = "CaskaydiaCove Nerd Font 11";
-            icon-theme = "Tela";
-          };
-
-          "org/gnome/desktop/background" = {
-            picture-uri = "";
-            picture-uri-dark = "";
-            primary-color = "#000000";
-          };
-
-          "org/gnome/desktop/input-sources" = {
-            sources = [ (mkTuple [ "xkb" "us+intl" ]) ];
-            xkb-options = [ "altwin:swap_lalt_lwin" ];
-          };
-
-          "org/gnome/desktop/session" = {
-            idle-delay = mkUint32 0;
-          };
-
-          "org/gnome/desktop/wm/keybindings" = {
-            close = [ "<Super>q" ];
-            toggle-maximized = [ "<Super>f" ];
-            toggle-fullscreen = [ "<Super><Shift>f" ];
-            move-to-center = [ "<Super>c" ];
-            move-to-monitor-left = [ "<Super><Shift>Left" ];
-            move-to-monitor-right = [ "<Super><Shift>Right" ];
-            switch-to-workspace-left = [ "<Super><Control>Left" ];
-            switch-to-workspace-right = [ "<Super><Control>Right" ];
-            move-to-workspace-left = [ "<Super><Shift><Control>Left" ];
-            move-to-workspace-right = [ "<Super><Shift><Control>Right" ];
-            switch-applications = [ "<Alt>Tab" "<Super>Tab" ];
-            switch-applications-backward = [ "<Shift><Alt>Tab" "<Shift><Super>Tab" ];
-            cycle-windows-backward = [ ];
-          } // (lib.genAttrs
-            (map (i: "switch-to-workspace-${i}") (lib.range 1 9))
-            (i: [ "<Super>${builtins.substring 6 1 i}" ])
-          ) // (lib.genAttrs
-            (map (i: "move-to-workspace-${i}") (lib.range 1 9))
-            (i: [ "<Super><Shift>${builtins.substring 6 1 i}" ])
-          );
-
-          "org/gnome/shell/keybindings" = {
-            toggle-overview = [ "<Super>a" ];
-            show-screenshot-ui = [ "<Super><Shift>s" ];
-            screenshot = [ "<Super><Shift>p" ];
-            screenshot-window = [ "<Super><Shift>w" ];
-            screen-brightness-up = [ "XF86MonBrightnessUp" ];
-            screen-brightness-down = [ "XF86MonBrightnessDown" ];
-          } // (lib.genAttrs
-            (map (i: "switch-to-application-${i}") (lib.range 1 9))
-            (_: [ ])
-          );
-
-          "org/gnome/mutter" = {
-            dynamic-workspaces = true;
-            auto-maximize = false;
-          };
-
-          "org/gnome/shell" = {
-            enabled-extensions = [
-              "appindicatorsupport@rgcjonas.gmail.com"
-              "copyous@boerdereinar.dev"
-              "display-brightness-ddcutil@themightydeity.github.com"
-              "quick-settings-audio-panel@rayzeq.github.com"
-              "dash-to-dock@micxgx.gmail.com"
-              "pop-shell@system76.com"
-              "caffeine@patapon.info"
-            ];
-          };
-
-          "org/gnome/settings-daemon/plugins/media-keys" = {
-            email = [ ];
-            volume-up = [ "XF86AudioRaiseVolume" ];
-            volume-down = [ "XF86AudioLowerVolume" ];
-            volume-mute = [ "XF86AudioMute" ];
-            mic-mute = [ "XF86AudioMicMute" ];
-            play = [ "XF86AudioPlay" ];
-            stop = [ "XF86AudioStop" ];
-            previous = [ "XF86AudioPrev" ];
-            next = [ "XF86AudioNext" ];
-            screensaver = [ "<Super>l" ];
-            custom-keybindings = [
-              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
-              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/file-manager/"
-            ];
-            "custom-keybindings/terminal/binding" = "<Super>Return";
-            "custom-keybindings/terminal/command" = "kgx";
-            "custom-keybindings/terminal/name" = "Terminal";
-            "custom-keybindings/terminal/enable-in-lockscreen" = false;
-            "custom-keybindings/file-manager/binding" = "<Super>e";
-            "custom-keybindings/file-manager/command" = "nautilus";
-            "custom-keybindings/file-manager/name" = "File manager";
-            "custom-keybindings/file-manager/enable-in-lockscreen" = false;
-          };
-
-          "org/gnome/settings-daemon/plugins/power" = {
-            power-saver-profile-on-low-battery = true;
-            sleep-inactive-ac-type = "nothing";
-            sleep-inactive-ac-timeout = 900;
-            sleep-inactive-battery-type = "suspend";
-            sleep-inactive-battery-timeout = 300;
-            idle-dim = true;
-            idle-brightness = 30;
-            power-button-action = "interactive";
-          };
-
-          "org/gnome/shell/extensions/pop-shell" = {
-            tile-by-default = false;
-          };
-
-          "org/gnome/shell/extensions/display-brightness-ddcutil" = {
-            button-location = 1.0;
-            hide-system-indicator = true;
-            show-display-name = false;
-          };
-
-          "org/gnome/shell/extensions/dash-to-dock" = {
-            dock-position = "BOTTOM";
-            hot-keys = false;
-            hotkeys-show-dock = false;
-            hotkeys-overlay = false;
-          };
-
-          "org/gnome/nautilus/preferences" = {
-            default-folder-viewer = "list-view";
-          };
-          "org/gnome/nautilus/list-view" = {
-            use-tree-view = true;
-            default-zoom-level = "medium";
-          };
+      settings = {
+        "org/gnome/shell" = {
+          enabled-extensions = [
+            "appindicatorsupport@rgcjonas.gmail.com"
+            "copyous@boerdereinar.dev"
+            "display-brightness-ddcutil@themightydeity.github.com"
+            "quick-settings-audio-panel@rayzeq.github.com"
+            "dash-to-dock@micxgx.gmail.com"
+            "pop-shell@system76.com"
+            "caffeine@patapon.info"
+          ];
         };
+      };
+    }
+    {
+      keyfiles = with pkgs; [
+        (runCommand "gnome-dconf-keyfiles" {
+          preferLocalBuild = true;
+        } (let
+          kdir = "keyfiles";
+        in ''
+          mkdir -p ${kdir}
+
+          cat > ${kdir}/00-appearance.conf << 'CONF'
+          [org/gnome/desktop/interface]
+          color-scheme='prefer-dark'
+          accent-color='teal'
+          font-name='CaskaydiaCove Nerd Font 11'
+          icon-theme='Tela'
+
+          [org/gnome/desktop/background]
+          picture-uri=""
+          picture-uri-dark=""
+          primary-color="#000000"
+          CONF
+
+          cat > ${kdir}/01-keybindings.conf << 'CONF'
+          [org/gnome/desktop/input-sources]
+          sources=[('xkb', 'us+intl')]
+          xkb-options=['altwin:swap_lalt_lwin']
+
+          [org/gnome/desktop/wm/keybindings]
+          close=['<Super>q']
+          toggle-maximized=['<Super>f']
+          toggle-fullscreen=['<Super><Shift>f']
+          move-to-center=['<Super>c']
+          move-to-monitor-left=['<Super><Control><Shift>Left']
+          move-to-monitor-right=['<Super><Control><Shift>Right']
+          switch-to-workspace-left=['<Super><Control>Left']
+          switch-to-workspace-right=['<Super><Control>Right']
+          move-to-workspace-left=['<Super><Shift><Control>Left']
+          move-to-workspace-right=['<Super><Shift><Control>Right']
+          switch-applications=['<Alt>Tab', '<Super>Tab']
+          switch-applications-backward=['<Shift><Alt>Tab', '<Shift><Super>Tab']
+          cycle-windows-backward=@as []
+          switch-to-workspace-1=['<Super>1']
+          switch-to-workspace-2=['<Super>2']
+          switch-to-workspace-3=['<Super>3']
+          switch-to-workspace-4=['<Super>4']
+          switch-to-workspace-5=['<Super>5']
+          switch-to-workspace-6=['<Super>6']
+          switch-to-workspace-7=['<Super>7']
+          switch-to-workspace-8=['<Super>8']
+          switch-to-workspace-9=['<Super>9']
+          move-to-workspace-1=['<Super><Shift>1']
+          move-to-workspace-2=['<Super><Shift>2']
+          move-to-workspace-3=['<Super><Shift>3']
+          move-to-workspace-4=['<Super><Shift>4']
+          move-to-workspace-5=['<Super><Shift>5']
+          move-to-workspace-6=['<Super><Shift>6']
+          move-to-workspace-7=['<Super><Shift>7']
+          move-to-workspace-8=['<Super><Shift>8']
+          move-to-workspace-9=['<Super><Shift>9']
+
+          [org/gnome/shell/keybindings]
+          toggle-overview=['<Super>a']
+          show-screenshot-ui=['<Super><Shift>s']
+          screenshot=['<Super><Shift>p']
+          screenshot-window=['<Super><Shift>w']
+          screen-brightness-up=['XF86MonBrightnessUp']
+          screen-brightness-down=['XF86MonBrightnessDown']
+          switch-to-application-1=@as []
+          switch-to-application-2=@as []
+          switch-to-application-3=@as []
+          switch-to-application-4=@as []
+          switch-to-application-5=@as []
+          switch-to-application-6=@as []
+          switch-to-application-7=@as []
+          switch-to-application-8=@as []
+          switch-to-application-9=@as []
+
+          [org/gnome/mutter]
+          dynamic-workspaces=true
+          auto-maximize=false
+          CONF
+
+          cat > ${kdir}/02-media-keys.conf << 'CONF'
+          [org/gnome/settings-daemon/plugins/media-keys]
+          email=@as []
+          volume-up=['XF86AudioRaiseVolume']
+          volume-down=['XF86AudioLowerVolume']
+          volume-mute=['XF86AudioMute']
+          mic-mute=['XF86AudioMicMute']
+          play=['XF86AudioPlay']
+          stop=['XF86AudioStop']
+          previous=['XF86AudioPrev']
+          next=['XF86AudioNext']
+          screensaver=['<Super>l']
+          custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/file-manager/']
+
+          [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal]
+          binding='<Super>Return'
+          command='kgx'
+          name='Terminal'
+          enable-in-lockscreen=false
+
+          [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/file-manager]
+          binding='<Super>e'
+          command='nautilus'
+          name='File manager'
+          enable-in-lockscreen=false
+
+          [org/gnome/settings-daemon/plugins/power]
+          power-saver-profile-on-low-battery=true
+          sleep-inactive-battery-type='suspend'
+          sleep-inactive-battery-timeout=uint32 300
+          idle-dim=true
+          idle-brightness=uint32 30
+          power-button-action='interactive'
+
+          [org/gnome/desktop/session]
+          idle-delay=uint32 900
+          lock-enabled=false
+          lock-delay=uint32 0
+          CONF
+
+          cat > ${kdir}/03-extensions.conf << 'CONF'
+          [org/gnome/shell/extensions/pop-shell]
+          tile-by-default=false
+          tile-enter=@as []
+          tile-move-left-global=['<Super><Shift>Left']
+          tile-move-right-global=['<Super><Shift>Right']
+          tile-move-up-global=['<Super><Shift>Up']
+          tile-move-down-global=['<Super><Shift>Down']
+          toggle-tiling=['<Super>t']
+
+          [org/gnome/shell/extensions/display-brightness-ddcutil]
+          button-location=1.0
+          hide-system-indicator=true
+          show-display-name=false
+
+          [org/gnome/shell/extensions/dash-to-dock]
+          dock-position='BOTTOM'
+          hot-keys=false
+          hotkeys-show-dock=false
+          hotkeys-overlay=false
+          CONF
+
+          cat > ${kdir}/04-nautilus.conf << 'CONF'
+          [org/gnome/nautilus/preferences]
+          default-folder-viewer='list-view'
+
+          [org/gnome/nautilus/list-view]
+          use-tree-view=true
+          default-zoom-level='medium'
+          CONF
+
+          cp -r ${kdir} $out
+        ''))
+      ];
     }
   ];
 }
