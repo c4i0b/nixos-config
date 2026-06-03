508273050119f9f23523a9e71f4c96a54a92eefd
Caio <cbrunofb@gmail.com>
2026-06-03 18:37:38 -0300

    input: use fcitx5 wayland frontend and set keyboard profile to us(intl)

--- nixos/input.nix
+++ nixos/input.nix
@@ -3,15 +3,32 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.settings.addons = {
-      notificationitem = {
-        globalSection.Enabled = "False";
+    fcitx5.waylandFrontend = true;
+    fcitx5.settings = {
+      inputMethod = {
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us(intl)";
+          DefaultIM = "keyboard-us-intl";
+        };
+        "Groups/0/Items/0" = {
+          Name = "keyboard-us-intl";
+          Layout = "";
+        };
+        GroupOrder = {
+          "0" = "Default";
+        };
       };
-      wayland = {
-        globalSection.AllowOverridingSystemXKBSettings = "False";
-      };
-      xcb = {
-        globalSection.AllowOverridingSystemXKBSettings = "False";
+      addons = {
+        notificationitem = {
+          globalSection.Enabled = "False";
+        };
+        wayland = {
+          globalSection.AllowOverridingSystemXKBSettings = "False";
+        };
+        xcb = {
+          globalSection.AllowOverridingSystemXKBSettings = "False";
+        };
       };
     };
   };
