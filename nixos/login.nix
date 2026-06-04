175f599b4eeff361acb6388d4a6633300140bcea
Caio <cbrunofb@gmail.com>
2026-06-04 16:17:04 -0300

    feat: enable Ly auto-login for GNOME

--- nixos/login.nix
+++ nixos/login.nix
@@ -1,9 +1,17 @@
 # Display manager, auto-login, and session selection.
 { config, lib, pkgs, ... }: {
-  services.displayManager.ly = {
-    enable = true;
-    settings = {
-      animation = "matrix";
+  services.displayManager = {
+    ly = {
+      enable = true;
+      settings = {
+        animation = "matrix";
+      };
+    };
+
+    defaultSession = "gnome";
+    autoLogin = {
+      enable = true;
+      user = "caio";
     };
   };
 }
