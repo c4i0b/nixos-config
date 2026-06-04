1fc9a0036ec0634364fc781ce86f6cbd1af1b02e
Caio <cbrunofb@gmail.com>
2026-06-04 16:34:49 -0300

    perf: skip NetworkManager-wait-online at boot (~3s faster)

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,9 +1,14 @@
 # Hostname, network manager, and firewall.
-{...}: {
+{pkgs, lib, ...}: {
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
     firewall.enable = true;
+  };
+
+  systemd.services.NetworkManager-wait-online = {
+    enable = true;
+    wantedBy = lib.mkForce [];
   };
 
   services.tailscale = {
