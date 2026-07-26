8bca0c65d05adc9bfea5b3126558d01b0c702a48
Caio <cbrunofb@gmail.com>
2026-07-26 16:18:34 -0300

    fix topgrade timer: fix PATH, disable nix step, add cleanup

--- configuration.nix
+++ configuration.nix
@@ -259,10 +259,11 @@
   # -- User services (topgrade) --
   systemd.user.services.topgrade-user = {
     description = "Topgrade - user updates";
+    path = [ "/run/current-system/sw" ];
     serviceConfig = {
       Type = "oneshot";
       ExecCondition = "${pkgs.iputils}/bin/ping -c 1 -W 5 8.8.8.8";
-      ExecStart = "${pkgs.topgrade}/bin/topgrade --disable system --disable firmware --yes --no-ask-retry --auto-retry 3";
+      ExecStart = "${pkgs.topgrade}/bin/topgrade --disable system --disable firmware --disable nix --yes --no-ask-retry --auto-retry 3 --cleanup";
       Nice = 19;
       IOSchedulingClass = "idle";
       CPUSchedulingPolicy = "idle";
